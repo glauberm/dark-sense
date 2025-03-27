0bb0b4b0c3c582cdc3ea73242aa6c047f5afa157
Glauber Mota <glaubernm@gmail.com>
2025-03-26 21:33:44 -0300

    chore: add new icon types to component cache

--- src/lib/icons.js
+++ src/lib/icons.js
@@ -14,24 +14,39 @@ import { icon as OuiIconAppSecurity } from '@opensearch-project/oui/es/component
 import { icon as OuiIconAppSpaces } from '@opensearch-project/oui/es/components/icon/assets/app_spaces';
 import { icon as OuiIconAppVisualize } from '@opensearch-project/oui/es/components/icon/assets/app_visualize';
 import { icon as OuiIconAppWatches } from '@opensearch-project/oui/es/components/icon/assets/app_watches';
+import { icon as OuiIconArrowDown } from '@opensearch-project/oui/es/components/icon/assets/arrow_down';
+import { icon as OuiIconArrowLeft } from '@opensearch-project/oui/es/components/icon/assets/arrow_left';
+import { icon as OuiIconArrowRight } from '@opensearch-project/oui/es/components/icon/assets/arrow_right';
 import { icon as OuiIconBell } from '@opensearch-project/oui/es/components/icon/assets/bell';
+import { icon as OuiIconCheck } from '@opensearch-project/oui/es/components/icon/assets/check';
 import { icon as OuiIconCross } from '@opensearch-project/oui/es/components/icon/assets/cross';
 import { icon as OuiIconDockedLeft } from '@opensearch-project/oui/es/components/icon/assets/dockedLeft';
+import { icon as OuiIconDot } from '@opensearch-project/oui/es/components/icon/assets/dot';
+import { icon as OuiIconEmpty } from '@opensearch-project/oui/es/components/icon/assets/empty';
 import { icon as OuiIconHelp } from '@opensearch-project/oui/es/components/icon/assets/help';
 import { icon as OuiIconLayers } from '@opensearch-project/oui/es/components/icon/assets/layers';
 import { icon as OuiIconMenu } from '@opensearch-project/oui/es/components/icon/assets/menu';
 import { icon as OuiIconMenuLeft } from '@opensearch-project/oui/es/components/icon/assets/menuLeft';
 import { icon as OuiIconMenuRight } from '@opensearch-project/oui/es/components/icon/assets/menuRight';
 import { icon as OuiIconReporter } from '@opensearch-project/oui/es/components/icon/assets/reporter';
+import { icon as OuiIconSortDown } from '@opensearch-project/oui/es/components/icon/assets/sort_down';
+import { icon as OuiIconSortUp } from '@opensearch-project/oui/es/components/icon/assets/sort_up';
+import { icon as OuiIconTrash } from '@opensearch-project/oui/es/components/icon/assets/trash';
 
 appendIconComponentCache({
   anomalyDetection: OuiIconAnomalyDetection,
+  arrowDown: OuiIconArrowDown,
+  arrowLeft: OuiIconArrowLeft,
+  arrowRight: OuiIconArrowRight,
   bell: OuiIconBell,
+  check: OuiIconCheck,
   cross: OuiIconCross,
   dashboardApp: OuiIconAppDashboard,
   devToolsApp: OuiIconAppDevtools,
   discoverApp: OuiIconAppDiscover,
   dockedLeft: OuiIconDockedLeft,
+  dot: OuiIconDot,
+  empty: OuiIconEmpty,
   help: OuiIconHelp,
   layers: OuiIconLayers,
   machineLearningApp: OuiIconAppMachineLearning,
@@ -45,7 +60,10 @@ appendIconComponentCache({
   reportingApp: OuiIconAppReporting,
   searchProfilerApp: OuiIconAppSearchProfiler,
   securityApp: OuiIconAppSecurity,
+  sortDown: OuiIconSortDown,
+  sortUp: OuiIconSortUp,
   spacesApp: OuiIconAppSpaces,
+  trash: OuiIconTrash,
   visualizeApp: OuiIconAppVisualize,
   watchesApp: OuiIconAppWatches,
 });
